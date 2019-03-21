62af3eaa90d48f8c02ceb4490f369b4791f2cd63
Joanna Stiller <joannastiller@yahoo.com>
2019-03-21 21:16:30 +0100

    Add function for summary and cost per person

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 const menuItems = document.querySelectorAll('.item');
 const listOfProducts = document.querySelector('.list-of-products');
 const sum = document.querySelector('.sum');
+const guestNumber = document.querySelector('input');
+const result = document.querySelector('.result');
 
-//Usuwanie dań z listy
-const removeItem = (e) => {
-    e.target.parentNode.remove()
-}
 
-//Dodawanie dań do listy
+
+//Dodawanie dań do listy i ich usuwanie
 menuItems.forEach(menuItem => menuItem.addEventListener('click', function () {
-    const itemName = menuItem.getAttribute('data-name');
-    let itemPrice = menuItem.getAttribute('data-price');
+    let itemName = menuItem.getAttribute('data-name');
+    let itemPrice = Number(menuItem.getAttribute('data-price'));
     const newItem = document.createElement('p');
     listOfProducts.appendChild(newItem);
     newItem.innerHTML = `${itemName} ${itemPrice} zł <button>x</button>`;
 
+
+    const removeItem = (e) => {
+        e.target.parentNode.remove();
+        sum.textContent = Number(sum.textContent) - itemPrice;
+    }
     newItem.querySelector('button').addEventListener('click', removeItem);
 
-    // let itemPriceNum = Number(itemPrice);
-    // itemPriceNum += itemPriceNum;
-    sum.textContent = Number(itemPrice);
-
-
+    sum.textContent = Number(sum.textContent) + itemPrice;
 }))
+
+//Obliczanie kwoty dla każdej osoby
+guestNumber.addEventListener('input', function () {
+    let costPerPerson = (Number(sum.textContent) / guestNumber.value).toFixed(1);
+    result.textContent = costPerPerson;
+})
